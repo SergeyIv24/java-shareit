7140f884dcd7d950835c4859b4cd636a7de80ec8
Sergey Ivanov <Reger21a@yandex.ru>
2024-07-19 18:20:30 +0300

    add tests for ItemContr and userController

--- src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -8,16 +8,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.item.dto.CommentsDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoWithDates;
+import ru.practicum.shareit.item.model.Item;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,63 +37,197 @@ public class ItemControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    private static ItemDto item1;
-    private static ItemDto itemAsResponseToRequest;
+    private static ItemDto itemDto1;
+    private static ItemDto itemAsResponseToRequestDto;
+
+    private static CommentsDto commentsDto;
+    private static LocalDateTime now = LocalDateTime.now();
 
     @BeforeAll
     static void initializeSomeItems() {
-        item1 = new ItemDto();
-        item1.setId(1L);
-        item1.setName("Пакет для мусора");
-        item1.setDescription("В пакете можно вынести мусор");
-        item1.setAvailable(true);
+        itemDto1 = new ItemDto();
+        itemDto1.setId(1L);
+        itemDto1.setName("Пакет для мусора");
+        itemDto1.setDescription("Один пакет");
+        itemDto1.setAvailable(true);
 
-        itemAsResponseToRequest = new ItemDto();
-        itemAsResponseToRequest.setId(2L);
-        itemAsResponseToRequest.setName("Пакет");
-        itemAsResponseToRequest.setDescription("Для других пакетов");
-        itemAsResponseToRequest.setAvailable(true);
-        itemAsResponseToRequest.setRequestId(1L);
+        itemAsResponseToRequestDto = new ItemDto();
+        itemAsResponseToRequestDto.setId(2L);
+        itemAsResponseToRequestDto.setName("Пакет");
+        itemAsResponseToRequestDto.setDescription("Для других пакетов");
+        itemAsResponseToRequestDto.setAvailable(true);
+        itemAsResponseToRequestDto.setRequestId(1L);
+    }
+
+    @BeforeAll
+    static void createComment() {
+        commentsDto = new CommentsDto();
+        commentsDto.setId(1L);
+        commentsDto.setText("Some text");
+        commentsDto.setAuthorName("Some name");
+        commentsDto.setCreated(now);
     }
 
     @Test
     void shouldCreateItem() throws Exception {
         when(itemService.addItem(anyLong(), any()))
-                .thenReturn(item1);
+                .thenReturn(itemDto1);
 
         mvc.perform(post("/items")
-                .content(mapper.writeValueAsString(item1))
-                .header("X-Sharer-User-Id", 1L)
-                .characterEncoding(StandardCharsets.UTF_8)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(item1.getId()), Long.class))
-                .andExpect(jsonPath("$.name", is(item1.getName()), String.class))
-                .andExpect(jsonPath("$.description", is(item1.getDescription()), String.class))
-                .andExpect(jsonPath("$.available", is(item1.getAvailable()), Boolean.class));
-    }
-
-    @Test
-    void shouldCreateItemAsResponseToRequest() throws Exception {
-        when(itemService.addItem(anyLong(), any()))
-                .thenReturn(itemAsResponseToRequest);
-
-        mvc.perform(post("/items")
-                        .content(mapper.writeValueAsString(itemAsResponseToRequest))
+                        .content(mapper.writeValueAsString(itemDto1))
                         .header("X-Sharer-User-Id", 1L)
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(itemAsResponseToRequest.getId()), Long.class))
-                .andExpect(jsonPath("$.name", is(itemAsResponseToRequest.getName()), String.class))
-                .andExpect(jsonPath("$.description", is(itemAsResponseToRequest.getDescription()), String.class))
-                .andExpect(jsonPath("$.available", is(itemAsResponseToRequest.getAvailable()), Boolean.class))
-                .andExpect(jsonPath("$.requestId", is(itemAsResponseToRequest.getRequestId()), Long.class));
+                .andExpect(jsonPath("$.id", is(itemDto1.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(itemDto1.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(itemDto1.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(itemDto1.getAvailable()), Boolean.class));
     }
 
+    @Test
+    void shouldCreateItemAsResponseToRequest() throws Exception {
+        when(itemService.addItem(anyLong(), any()))
+                .thenReturn(itemAsResponseToRequestDto);
+
+        mvc.perform(post("/items")
+                        .content(mapper.writeValueAsString(itemAsResponseToRequestDto))
+                        .header("X-Sharer-User-Id", 1L)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemAsResponseToRequestDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(itemAsResponseToRequestDto.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(itemAsResponseToRequestDto.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(itemAsResponseToRequestDto.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.requestId", is(itemAsResponseToRequestDto.getRequestId()), Long.class));
+    }
+
+    @Test
+    void shouldUpdateItem() throws Exception {
+        itemDto1.setName("another item");
+        itemDto1.setDescription("it was updated");
+        when(itemService.updateItem(anyLong(), anyLong(), any()))
+                .thenReturn(itemDto1);
+
+        mvc.perform(patch("/items/1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Sharer-User-Id", 1L)
+                        .content(mapper.writeValueAsString(itemDto1)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(itemDto1.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(itemDto1.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(itemDto1.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(itemDto1.getAvailable()), Boolean.class));
+    }
+
+    @Test
+    void shouldReturnItemById() throws Exception {
+        Item item1 = ItemMapper.mapToItem(itemDto1);
+        item1.setId(itemDto1.getId());
+        ItemDtoWithDates item1WithDates = ItemMapper
+                .mapToItemDtoWithDates(item1, null, null, null);
+
+        when(itemService.getItemById(anyLong(), any(), anyLong()))
+                .thenReturn(item1WithDates);
+
+        mvc.perform(get("/items/1")
+                        .header("X-Sharer-User-Id", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(item1WithDates.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(item1WithDates.getName()), String.class))
+                .andExpect(jsonPath("$.description", is(item1WithDates.getDescription()), String.class))
+                .andExpect(jsonPath("$.available", is(item1WithDates.getAvailable()), Boolean.class));
+    }
+
+    @Test
+    void shouldReturnUsersItems() throws Exception {
+        Item item1 = ItemMapper.mapToItem(itemDto1);
+        item1.setId(itemDto1.getId());
+        Item itemAsResponseToRequest = ItemMapper.mapToItem(itemAsResponseToRequestDto);
+        itemAsResponseToRequest.setId(itemAsResponseToRequestDto.getId());
+        ItemDtoWithDates item1Dates =
+                ItemMapper.mapToItemDtoWithDates(item1, null, null, List.of(commentsDto, commentsDto));
+
+        ItemDtoWithDates itemAsResponseDates =
+                ItemMapper.mapToItemDtoWithDates(itemAsResponseToRequest, null, null, List.of(commentsDto));
+
+        when(itemService.getMyItems(anyLong(), any()))
+                .thenReturn(List.of(item1Dates, itemAsResponseDates));
+
+        mvc.perform(get("/items")
+                        .header("X-Sharer-User-Id", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id", is(item1Dates.getId()), Long.class))
+                .andExpect(jsonPath("$.[0].name", is(item1Dates.getName()), String.class))
+                .andExpect(jsonPath("$.[0].description", is(item1Dates.getDescription()), String.class))
+                .andExpect(jsonPath("$.[0].available", is(item1Dates.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.[0].comments.[0].id",
+                        is(item1Dates.getComments().get(0).getId()), Long.class))
+                .andExpect(jsonPath("$.[0].comments.[0].text",
+                        is(item1Dates.getComments().get(0).getText()), String.class))
+                .andExpect(jsonPath("$.[0].comments.[1].id",
+                        is(item1Dates.getComments().get(1).getId()), Long.class))
+                .andExpect(jsonPath("$.[0].comments.[1].text",
+                        is(item1Dates.getComments().get(1).getText()), String.class))
+                .andExpect(jsonPath("$.[1].id", is(itemAsResponseDates.getId()), Long.class))
+                .andExpect(jsonPath("$.[1].name", is(itemAsResponseDates.getName()), String.class))
+                .andExpect(jsonPath("$.[1].description",
+                        is(itemAsResponseDates.getDescription()), String.class))
+                .andExpect(jsonPath("$.[1].available",
+                        is(itemAsResponseDates.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.[1].comments.[0].id",
+                        is(itemAsResponseDates.getComments().get(0).getId()), Long.class))
+                .andExpect(jsonPath("$.[1].comments.[0].text",
+                        is(itemAsResponseDates.getComments().get(0).getText()), String.class));
 
 
+    }
 
+    @Test
+    void shouldSearch() throws Exception {
+
+        when(itemService.searchByRequest(anyString()))
+                .thenReturn(List.of(itemDto1, itemAsResponseToRequestDto));
+
+        mvc.perform(get("/items/search")
+                        .param("text", "Пакет")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id", is(itemDto1.getId()), Long.class))
+                .andExpect(jsonPath("$.[0].name", is(itemDto1.getName()), String.class))
+                .andExpect(jsonPath("$.[0].description", is(itemDto1.getDescription()), String.class))
+                .andExpect(jsonPath("$.[0].available", is(itemDto1.getAvailable()), Boolean.class))
+                .andExpect(jsonPath("$.[1].id", is(itemAsResponseToRequestDto.getId()), Long.class))
+                .andExpect(jsonPath("$.[1].name", is(itemAsResponseToRequestDto.getName()), String.class))
+                .andExpect(jsonPath("$.[1].description", is(itemAsResponseToRequestDto.getDescription()), String.class))
+                .andExpect(jsonPath("$.[1].available", is(itemAsResponseToRequestDto.getAvailable()), Boolean.class));
+    }
+
+    @Test
+    void shouldAddCommentToItem() throws Exception {
+
+        when(itemService.addComment(anyLong(), any(), anyLong()))
+                .thenReturn(commentsDto);
+
+        mvc.perform(post("/items/1/comment")
+                        .content(mapper.writeValueAsString(commentsDto))
+                        .header("X-Sharer-User-Id", 1L)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(commentsDto.getId()), Long.class))
+                .andExpect(jsonPath("$.text", is(commentsDto.getText()), String.class))
+                .andExpect(jsonPath("$.authorName", is(commentsDto.getAuthorName()), String.class))
+                .andExpect(jsonPath("$.created", is(commentsDto.getCreated().toString()), String.class));
+    }
 }
